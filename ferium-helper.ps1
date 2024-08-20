60bf32622f76b86b175e55b1602213ee67f80f36
s809 <43530948+s809@users.noreply.github.com>
2024-08-20 16:10:15 +0500

    Add PowerShell version

---
+++ ferium-helper.ps1
@@ -0,0 +1,183 @@
+#region Help
+function Show-Help {
+    Write-Host "Usage:"
+    Write-Host "    $PSCommandPath <source-profile> <target-version> <target-profile>"
+    Write-Host "    $PSCommandPath <source-profile> <target-version>"
+    Write-Host "    $PSCommandPath <target-profile>"
+    Write-Host ""
+    Write-Host "Arguments:"
+    Write-Host "    <source-profile>   : Name of the source profile (optional, only if target profile does not exist)"
+    Write-Host "    <target-version>   : Version of the game to target"
+    Write-Host "    <target-profile>   : Name of the target profile"
+    Write-Host ""
+    Write-Host "Description:"
+    Write-Host "    Copies the source profile to the target profile with the specified version, if it does not exist."
+    Write-Host "    If only two arguments are provided, it uses the first argument as the target version and the second as the target profile."
+    Write-Host "    If only one argument is provided, it performs an upgrade on the specified profile without copying from another profile."
+}
+#endregion
+
+
+#region Utils
+# Formats JSON in a nicer format than the built-in ConvertTo-Json does.
+function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
+    $indent = 0;
+    ($json -Split '\n' | % {
+        if ($_ -match '[\}\]]') {
+            # This line contains  ] or }, decrement the indentation level
+            $indent--
+        }
+        $line = (' ' * $indent * 2) + $_.TrimStart().Replace(':  ', ': ')
+        if ($_ -match '[\{\[]') {
+            # This line contains [ or {, increment the indentation level
+            $indent++
+        }
+        $line
+    }) -Join "`n"
+}
+
+function Create-Shortcut {
+    param (
+        [string]$Path, # The file/folder you want to create a shortcut for
+        [string]$Target   # The location where the shortcut will be created
+    )
+
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($Path)
+    $Shortcut.TargetPath = $Target
+    $Shortcut.Save()
+}
+#endregion
+
+
+#region Initialization
+if ($args.Count -eq 3) {
+    $SOURCE_PROFILE_NAME = $args[0]
+    $TARGET_VERSION = $args[1]
+    $TARGET_PROFILE_NAME = $args[2]
+}
+elseif ($args.Count -eq 2) {
+    $TARGET_VERSION = $args[0]
+    $TARGET_PROFILE_NAME = $args[1]
+}
+elseif ($args.Count -eq 1) {
+    $TARGET_VERSION = $args[0]
+    $TARGET_PROFILE_NAME = $TARGET_VERSION
+}
+else {
+    Show-Help
+    exit 1
+}
+
+# Determine the run directory and mods directory
+$GAME_ROOT = Get-Location
+Write-Host "Game directory: $GAME_ROOT"
+
+# Move to the script's directory
+Set-Location -Path (Get-Item -Path $PSCommandPath).DirectoryName
+Write-Host "Ferium directory: $(Get-Location)"
+
+# Configuration file path
+$CONFIG_FILE = "$HOME\.config\ferium\config.json"
+if (-not (Test-Path -Path $CONFIG_FILE)) {
+    Write-Host "File not found: $CONFIG_FILE"
+    exit 1
+}
+#endregion
+
+
+#region Check for ferium updates
+# Get the latest version from GitHub
+$latestReleaseUrl = "https://api.github.com/repos/gorilla-devs/ferium/releases/latest"
+$latestRelease = Invoke-RestMethod -Uri $latestReleaseUrl -UseBasicParsing
+$latestVersion = $latestRelease.tag_name -replace "v", ""
+
+# Check if the current version matches the latest version
+$updateRequired = $true
+if (Test-Path .\ferium.exe) {
+    $currentVersion = (.\ferium.exe --version) -replace "ferium ", ""
+    Write-Host "Current Ferium version: $currentVersion"
+    Write-Host "Latest Ferium version: $latestVersion"
+
+    if ($currentVersion -eq $latestVersion) {
+        $updateRequired = $false
+    }
+}
+
+# Download and extract the latest release if needed
+if ($updateRequired) {
+    $zipFileName = "ferium-windows-msvc.zip"
+
+    Write-Host "Downloading latest Ferium release..."
+    $assetUrl = $latestRelease.assets | Where-Object { $_.name -eq $zipFileName } | Select-Object -ExpandProperty browser_download_url
+    Invoke-WebRequest -Uri $assetUrl -OutFile $zipFileName
+    Expand-Archive -Path $zipFileName -DestinationPath . -Force
+    Remove-Item -Path $zipFileName
+    Write-Host "Done."
+}
+#endregion
+
+
+#region Copy profile data
+# Read profiles from the config file
+$CONFIG = Get-Content -Path $CONFIG_FILE | ConvertFrom-Json
+$PROFILES = $CONFIG.profiles
+
+# Copy source profile if target profile doesn't exist yet
+$TARGET_PROFILE = $PROFILES | Where-Object { $_.name -eq $TARGET_PROFILE_NAME }
+if (-not $TARGET_PROFILE -and $SOURCE_PROFILE_NAME) {
+    $SOURCE_PROFILE = $PROFILES | Where-Object { $_.name -eq $SOURCE_PROFILE_NAME }
+
+    if ($SOURCE_PROFILE) {
+        $SOURCE_ROOT = $SOURCE_PROFILE.output_dir -replace '\\mods$', ''
+
+        # Copy game settings and configs
+        Copy-Item -Path "$SOURCE_ROOT\options.txt", "$SOURCE_ROOT\servers.dat", "$SOURCE_ROOT\config" -Destination $GAME_ROOT -Recurse
+
+        # Create user mods folder
+        New-Item -ItemType Directory -Path "$GAME_ROOT\mods\user" -Force
+
+        # Create new profile based on source profile
+        $NEW_PROFILE = $SOURCE_PROFILE | ConvertTo-Json -Compress -Depth 100 | ConvertFrom-Json
+        $NEW_PROFILE.game_version = $TARGET_VERSION
+        $NEW_PROFILE.name = $TARGET_PROFILE_NAME
+
+        # Add new profile to profiles and sort
+        $PROFILES += $NEW_PROFILE
+        $PROFILES = $PROFILES | Sort-Object -Property name
+
+        # Update the profiles in the original JSON structure
+        $CONFIG.profiles = $PROFILES
+
+        # Write the updated JSON back to the file
+        $CONFIG | ConvertTo-Json -Depth 100 | Format-Json | Set-Content -Path $CONFIG_FILE
+
+        Write-Host "Profile copied from $SOURCE_PROFILE_NAME to $TARGET_PROFILE_NAME with version $TARGET_VERSION."
+    }
+    else {
+        Write-Host "Profile with name $SOURCE_PROFILE_NAME not found. Skipping copy."
+    }
+}
+#endregion
+
+
+#region Shortcuts
+# Create config shortcut file
+Remove-Item -Path ./config.json.lnk -Force -ErrorAction Ignore
+Create-Shortcut -Path ./config.json.lnk -Target $CONFIG_FILE
+
+# Create game directory shortcut
+Remove-Item -Path ./.minecraft.lnk -Force -ErrorAction Ignore
+Create-Shortcut -Path ./.minecraft.lnk -Target $GAME_ROOT
+#endregion
+
+
+#region Ferium commands
+# Perform profile switch, configure mods directory, and upgrade
+.\ferium.exe profile switch $TARGET_PROFILE_NAME
+.\ferium.exe profile configure --output-dir "$($GAME_ROOT)\mods"
+Start-Process powershell "-Command Start-Transcript -Path .\upgrade.log; ./ferium upgrade" -Wait
+[regex]::Replace((Get-Content .\upgrade.log -Delimiter "This will never appear"), "(\*{22}\r\n.*?\*{22}\r\n(.*?\r\n)?|PS>.*)", "", "Singleline") `
+    -replace "\u2713", "+" `
+    -replace "\u00d7", "-"
+#endregion
--- ferium-helper.sh
+++ ferium-helper.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Determine the run directory and mods directory
-GAME_ROOT="$PWD"
-echo "Game directory: $GAME_ROOT"
+#region Help
+show_help() {
+    echo "Usage:"
+    echo "    $0 <source-profile> <target-version> <target-profile>"
+    echo "    $0 <source-profile> <target-version>"
+    echo "    $0 <target-profile>"
+    echo ""
+    echo "Arguments:"
+    echo "    <source-profile>   : Name of the source profile (optional, only if target profile does not exist)"
+    echo "    <target-version>   : Version of the game to target"
+    echo "    <target-profile>   : Name of the target profile"
+    echo ""
+    echo "Description:"
+    echo "    Copies the source profile to the target profile with the specified version, if it does not exist."
+    echo "    If only two arguments are provided, it uses the first argument as the target version and the second as the target profile."
+    echo "    If only one argument is provided, it performs an upgrade on the specified profile without copying from another profile."
+}
+#endregion
 
-cd "$(dirname "$0")" || exit 1
-echo "Ferium directory: $PWD"
-
-CONFIG_FILE=~/.config/ferium/config.json
-if [ ! -f "$CONFIG_FILE" ]; then
-    echo "File not found: $CONFIG_FILE"
-    exit 1
-fi
-
+#region Initialization
 if [ -n "$3" ]; then
     SOURCE_PROFILE_NAME="$1"
     TARGET_VERSION="$2"
@@ -20,18 +27,67 @@ if [ -n "$3" ]; then
 elif [ -n "$2" ]; then
     TARGET_VERSION="$1"
     TARGET_PROFILE_NAME="$2"
-else
+elif [ -n "$1" ]; then
     TARGET_VERSION="$1"
     TARGET_PROFILE_NAME="$TARGET_VERSION"
+else
+    show_help
+    exit 1
+fi
+# Determine the run directory and mods directory
+GAME_ROOT="$PWD"
+echo "Game directory: $GAME_ROOT"
+
+# Move to the script's directory
+cd "$(dirname "$0")" || exit 1
+echo "Ferium directory: $PWD"
+
+# Configuration file path
+CONFIG_FILE=~/.config/ferium/config.json
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "File not found: $CONFIG_FILE"
+    exit 1
+fi
+#endregion
+
+#region Check for ferium updates
+# Get the latest version from GitHub
+LATEST_RELEASE_URL="https://api.github.com/repos/gorilla-devs/ferium/releases/latest"
+LATEST_RELEASE=$(curl -s "$LATEST_RELEASE_URL")
+LATEST_VERSION=$(echo "$LATEST_RELEASE" | jq -r .tag_name | sed 's/v//')
+
+# Check if the current version matches the latest version
+UPDATE_REQUIRED=true
+if [ -f ./ferium ]; then
+    CURRENT_VERSION=$(./ferium --version | sed 's/ferium //')
+    echo "Current Ferium version: $CURRENT_VERSION"
+    echo "Latest Ferium version: $LATEST_VERSION"
+
+    if [ "$CURRENT_VERSION" = "$LATEST_VERSION" ]; then
+        UPDATE_REQUIRED=false
+    fi
 fi
 
-# Read profiles from config file
+# Download and extract the latest release if needed
+if [ "$UPDATE_REQUIRED" = true ]; then
+    ZIP_FILE_NAME="ferium-linux.zip"
+
+    echo "Downloading latest Ferium release..."
+    ASSET_URL=$(echo "$LATEST_RELEASE" | jq -r '.assets[] | select(.name == "'$ZIP_FILE_NAME'").browser_download_url')
+    curl -L -o $ZIP_FILE_NAME "$ASSET_URL"
+    unzip -o $ZIP_FILE_NAME -d .
+    rm $ZIP_FILE_NAME
+    echo "Done."
+fi
+#endregion
+
+#region Copy profile data
+# Read profiles from the config file
 PROFILES=$(jq '.profiles' "$CONFIG_FILE")
 
 # Copy source profile if target profile doesn't exist yet
 TARGET_PROFILE=$(echo "$PROFILES" | jq --arg profile_name "$TARGET_PROFILE_NAME" '.[] | select(.name == $profile_name)')
 if [ -z "$TARGET_PROFILE" ] && [ -n "$SOURCE_PROFILE_NAME" ]; then
-    # Check if source profile exists
     SOURCE_PROFILE=$(echo "$PROFILES" | jq --arg profile_name "$SOURCE_PROFILE_NAME" '.[] | select(.name == $profile_name)')
 
     if [ -n "$SOURCE_PROFILE" ]; then
@@ -59,20 +115,24 @@ if [ -z "$TARGET_PROFILE" ] && [ -n "$SOURCE_PROFILE_NAME" ]; then
         echo "Profile with name $SOURCE_PROFILE_NAME not found. Skipping copy."
     fi
 fi
+#endregion
 
-# Symlink config file if not done yet
-if [ ! -f ./config.json ]; then
-    ln -s "$CONFIG_FILE" ./config.json
-fi
+#region Shortcuts
+# Symlink config file if not already done
+rm -f ./config.json
+ln -s "$CONFIG_FILE" ./config.json
 
 # Symlink game directory and create user mods folder if not exists
 rm -f ./_minecraft
 ln -s "$GAME_ROOT" ./_minecraft
+#endregion
 
+#region Ferium commands
 # Perform profile switch, configure mods directory, and upgrade
 ./ferium profile switch "$TARGET_PROFILE_NAME"
 ./ferium profile configure --output-dir "$GAME_ROOT/mods"
 stdbuf -o0 script --return --quiet -c  "./ferium upgrade" /dev/null 2>&1 \
     | sed -u -r "s/$(echo -ne '[\u2800-\u28ff].*?2K')//gm"
+#endregion
 
 exit 0
